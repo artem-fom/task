edc52597e1804d74e783694803bf5e8863f9e1e1
Артем Фомичев <arfomic@sfedu.ru>
2025-09-11 21:49:02 +0300

    Добавлены новые файлы

---
+++ git-task/zadanie2prog.pas
@@ -0,0 +1,14 @@
+﻿begin
+  var a := ReadInteger();
+  var flg := true;
+  if a<2 then print(false)
+  else begin
+  for var i := 2 to floor(sqrt(a)) do begin
+    if a mod i = 0 then begin
+      flg := false;
+      break;
+    end;
+  end;
+  print(flg);
+  end;
+end.
